9dede4cc63593b391554039e7547c83079371e44
Mute <mute.255@outlook.com>
2026-08-17 19:20:18 +0100

    Optimised prototype check count code

--- bbee_tracker/scripts/custom_util.lua
+++ bbee_tracker/scripts/custom_util.lua
@@ -27,11 +27,8 @@ function updateProtoCheckCount(elapsed)
 	end
 	next_update = next_update - elapsed
 	if next_update <= 0 then
-		for _, proto in ipairs(proto_list) do
-			local code = getProtoCodeName(proto)
-			updateLocationCount(code)
-		end
 		next_update = -1.0
+		updateLocationCount(code)
 	end
 end
 
@@ -41,25 +38,27 @@ end
 
 -- count how many checks are in logic for this prototype
 function updateLocationCount(proto)
-    local check_count = 0
+	local proto_check_count = {}
     for l, v in pairs(LOCATION_TO_ID_MAP) do
-        if getProtoCodeName(l:match("^(.-)/")) == proto then
-            local loc = Tracker:FindObjectForCode("@" .. l)
-            if loc.AccessibilityLevel == AccessibilityLevel.Normal then
-                check_count = check_count + loc.AvailableChestCount
-            end
-        end
+		local code = getProtoCodeName(l:match("^(.-)/"))
+		local loc = Tracker:FindObjectForCode("@" .. l)
+		if loc.AccessibilityLevel == AccessibilityLevel.Normal then
+			proto_check_count[code] = (proto_check_count[code] or 0) + loc.AvailableChestCount
+		end
     end
-    -- print(proto .. " - " .. proto:lower() .. ": " .. check_count)
-    local proto_obj = Tracker:FindObjectForCode(proto)
-    if proto_obj ~= nil then
-        proto_obj:SetOverlay(tostring(check_count))
-        if check_count == 0 then
-            proto_obj:SetOverlayColor("#cf1010")
-        else
-            proto_obj:SetOverlayColor("#20ff20")
-        end
-    end
+	for i, p in ipairs(proto_list) do
+		local code = getProtoCodeName(p)
+		local count = (proto_check_count[code] or 0)
+		local proto_obj = Tracker:FindObjectForCode(code)
+		if proto_obj ~= nil then
+			proto_obj:SetOverlay(tostring(count))
+			if count == 0 then
+				proto_obj:SetOverlayColor("#cf1010")
+			else
+				proto_obj:SetOverlayColor("#20ff20")
+			end
+		end
+	end
 end
 
 ScriptHost:AddOnFrameHandler("update_proto_check_count", updateProtoCheckCount)
